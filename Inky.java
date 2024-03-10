d639242bea0242095c8157ba507a43c9affe1372
SubodhaB <161959079+SubodhaB@users.noreply.github.com>
2024-03-10 22:32:42 +0530

    Add files via upload

---
+++ Inky.java
@@ -0,0 +1,18 @@
+public class Inky{
+    public Inky(String x){
+        System.out.println(x);
+    }
+    public Inky(String pinky, String blinky, String rinky){
+        System.out.println(blinky+" "+pinky);
+        System.out.println(pinky+" "+rinky);
+        System.out.println(blinky+" "+pinky+" "+rinky);
+    }
+    public static void main(String[] args){
+        String pinky="Donky";
+        String blinky="Pinky";
+        String rinky="Monkey";
+
+    Inky ponky=new Inky(pinky);
+    Inky ponky2=new Inky(pinky, blinky, rinky);
+    }
+}
